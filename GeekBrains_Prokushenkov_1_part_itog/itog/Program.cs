aa5922e49e8b4a6d83856abc9beac2626e0a0ff4
Kirilll Proushenkov <95954756+kirill867@users.noreply.github.com>
2023-10-30 11:52:59 +0300

    Add files via upload

---
+++ GeekBrains_Prokushenkov_1_part_itog/itog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("длина массива: ");
+            int arrayLen = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[arrayLen];
+
+            for(int i = 0; i < arrayLen; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+
+            PrintItogStrings(array);
+        }
+
+        private static void PrintItogStrings(string[] array)
+        {
+            Console.WriteLine("--------------");
+            for (int i = 0; i < array.Length; i++)
+                if (array[i].Length <= 3)
+                    Console.WriteLine(array[i]);
+        }
+    }
+}
